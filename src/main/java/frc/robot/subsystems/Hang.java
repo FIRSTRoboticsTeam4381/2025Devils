44b99a7b716f2d49ba7d8009aa82a1f9365ac7e2
Garmonzaloni <eli.sealy2@gmail.com>
2025-01-25 13:28:11 -0500

    Barebones Hang file
    just motor and config

---
+++ src/main/java/frc/robot/subsystems/Hang.java
@@ -0,0 +1,40 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.subsystems;
+
+import com.revrobotics.spark.SparkBase.PersistMode;
+import com.revrobotics.spark.SparkBase.ResetMode;
+import com.revrobotics.spark.SparkFlex;
+import com.revrobotics.spark.SparkLowLevel.MotorType;
+import com.revrobotics.spark.SparkMax;
+import com.revrobotics.spark.config.SparkBaseConfig.IdleMode;
+import com.revrobotics.spark.config.SparkMaxConfig;
+
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+public class Hang extends SubsystemBase 
+{
+
+    public SparkMax hang1;
+  
+
+  public Hang() 
+  {
+      hang1 = new SparkMax(64, MotorType.kBrushless);
+
+
+    SparkMaxConfig hang1Config = new SparkMaxConfig();
+      hang1Config
+        .smartCurrentLimit(10)
+        .idleMode(IdleMode.kCoast);
+
+    hang1.configure(hang1Config, ResetMode.kResetSafeParameters, PersistMode.kPersistParameters);
+  }
+
+  @Override
+  public void periodic() {
+    // This method will be called once per scheduler run
+  }
+}
