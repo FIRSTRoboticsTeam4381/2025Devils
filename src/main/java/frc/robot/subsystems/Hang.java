48d9c5c4e6487afa5597dca731c9c673b911cc8b
John <johnny@leathermans.net>
2025-01-30 18:33:08 -0500

    Added commands to hang
    pretty sure thats what we needed

--- src/main/java/frc/robot/subsystems/Hang.java
+++ src/main/java/frc/robot/subsystems/Hang.java
@@ -7,17 +7,22 @@ package frc.robot.subsystems;
 import com.revrobotics.spark.SparkBase.PersistMode;
 import com.revrobotics.spark.SparkBase.ResetMode;
 import com.revrobotics.spark.SparkLowLevel.MotorType;
+import com.pathplanner.lib.events.CancelCommandEvent;
+import com.revrobotics.AbsoluteEncoder;
 import com.revrobotics.spark.SparkMax;
 import com.revrobotics.spark.config.SparkBaseConfig.IdleMode;
 import com.revrobotics.spark.config.SparkMaxConfig;
 
+import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.commands.SparkPosition;
 
 public class Hang extends SubsystemBase 
 {
 
     public SparkMax hang1;
-  
+
+    public AbsoluteEncoder position;
 
   public Hang() 
   {
@@ -30,6 +35,20 @@ public class Hang extends SubsystemBase
 
     hang1.configure(hang1Config, ResetMode.kResetSafeParameters, PersistMode.kPersistParameters);
   }
+
+  public Command hangPosition(double position) {
+    return new SparkPosition(hang1, position, 0, null).withName("Hang to " + position); // Will add positions later
+  }
+
+  public Command hang() {
+    return hangPosition(0).withName("Hang");
+  }
+
+  public Command unhang() {
+    return hangPosition(0).withName("Un Hang");
+  }
+
+
 
   @Override
   public void periodic() 
