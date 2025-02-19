b15247888208d9e0048e16d8d82c8508b5db1551
Sam <sam.maddox1@yahoo.com>
2025-02-18 23:39:31 -0500

    Fix swerve odometry pt 2

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -82,7 +82,7 @@ public final class Constants {
         public static final double maxAngularVelocity = 10; // Formally 11.5 (Unit is Omega Radians per sec)
 
         /* Motor Inverts */
-        public static final boolean driveMotorInvert = false;
+        public static final boolean driveMotorInvert = true;
         public static final boolean angleMotorInvert = true;
 
         /* Angle Encoder Invert */
